ff2f3ae159262f2aaa05591c307b4048c2784e1a
kazukiogushi <asahisoccer71@gmail.com>
2020-08-09 22:39:00 +0900

    a

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.i18n.default_locale = :ja
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
